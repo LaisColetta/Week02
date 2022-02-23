99150da19deaab2346f8c4661db1e23ee88a6994
LaisColetta <99751201+LaisColetta@users.noreply.github.com>
2022-02-23 18:51:32 +0000

    Add files via upload

---
+++ readName.py
@@ -0,0 +1,8 @@
+#Progrqam that reads in a name and print it out
+#Author: Lais Coletta
+
+name = input('Enter your name: ')
+print ('Hello ' + name)
+
+age = input('what is your age?: ')
+print ('your age is {}'.format(age))
