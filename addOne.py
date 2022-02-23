8b43a49fb80f0dc12e20aaf549cca5a424207352
LaisColetta <99751201+LaisColetta@users.noreply.github.com>
2022-02-23 18:49:06 +0000

    Delete addOne.py

--- addOne.py
+++
@@ -1,8 +0,0 @@
-# read in a number and add one
-#Author: Lais
-
-#we must convert the string that input returns to an int
-number = int(input('please enter a number: '))
-newNumber = number + 1
-print ('{} plus one is {}'.format(number,newNumber))
-
